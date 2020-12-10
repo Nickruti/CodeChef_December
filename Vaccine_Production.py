6458ef3694d470292f92eb44f7c94ed207b00f36
Kruti Patel <37500239+Nickruti@users.noreply.github.com>
2020-12-10 07:00:18 +0530

    Update Vaccine_Production.py

--- Vaccine_Production.py
+++ Vaccine_Production.py
@@ -1,19 +1,27 @@
 # cook your dish here
 import math
 arr = list(map(int, input().split()))
+days = 1
 if arr[0] == arr[2] == 1:
     days = math.ceil(arr[4]/(arr[1] + arr[3]))
 else:
-    if arr[0] > arr[2]:
-        day2 = arr[0] - arr[2]
-        sum1 = day2 * arr[3]
-        day1 = arr[2] - 1
-
-    else:
-        day2 = arr[2] - arr[0]
-        sum1 = day2 * arr[1]
-        day1 = arr[0] - 1
-    days = day1 + day2 + math.ceil((arr[4]-sum1)/(arr[1] + arr[3]))
+    cntV = 0
+    days = min(arr[0], arr[2])-1
+    d1 = arr[0]
+    d2 = arr[2]
+    while cntV < arr[4]:
+        if d1 < d2:
+            cntV = cntV + arr[1]
+            days += 1
+            d1 += 1
+            
+        elif d2 < d1:
+            cntV = cntV + arr[3]
+            days += 1
+            d2 += 1
+            
+        else:
+            cntV = cntV + arr[1] + arr[3]
+            days += 1
+        
 print(days)
-    
-
